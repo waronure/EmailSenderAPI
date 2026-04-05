2b53bff98f8f94ae2877982e87aaa0332b8fe09a
Warona <mogolwanew@gmail.com>
2026-04-05 21:43:35 +0200

    Added connection string and Email model

---
+++ Models/Email.cs
@@ -0,0 +1,6 @@
+public class Email{
+    public required string Body{get;set;}
+    public required string FromEmail {get; set;}
+    public required string ToEmail {get; set;}
+    public required string SmtpMessage {get; set;}
+}
